890e79d49c5e55dcd307682694c420c939df6a9e
Gabrielyoda <yodaegan@gmail.com>
2020-06-06 20:10:22 +0700

    upload gambar

---
+++ app/src/main/java/com/example/uploadgambarretrofit/network/Config.java
@@ -0,0 +1,8 @@
+package com.example.uploadgambarretrofit.network;
+
+public class Config {
+    public static final String BASE_URL = "http://192.168.43.131/"; // Your local IP Address
+        public static final String API_DIR = "Folder_API/gambar2";
+
+    public static final String API_UPLOAD = BASE_URL + API_DIR + "/upload.php";
+}
